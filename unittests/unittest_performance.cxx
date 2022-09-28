aaf8844ead636c3542a37d5195dd90a37b4f6821
Matthias Möller <m.moller@tudelft.nl>
2022-09-28 22:35:40 +0200

    Updated performance unittests

--- unittests/unittest_performance.cxx
+++ unittests/unittest_performance.cxx
@@ -24,7 +24,7 @@ TEST(Performance, MatmulTensorLayout)
   iganet::core<double> core_;
   
   for (short_t n : {2, 3, 4, 5}) {
-    for (short_t m : {100, 500, 1000, 5000, 10000, 50000, 100000}) {
+    for (int64_t m : {100, 500, 1000, 5000, 10000, 50000, 100000}) {
 
       {
         torch::Tensor a = torch::ones({n,m}, core_.options());
@@ -37,8 +37,8 @@ TEST(Performance, MatmulTensorLayout)
         auto t2 = std::chrono::high_resolution_clock::now();
         
         std::cout << "("
-                  << std::right << std::setw(6) << n << ","
-                  << std::right << std::setw(6) << m << ") "
+                  << std::right << std::setw(8) << n << ","
+                  << std::right << std::setw(8) << m << ") "
                   << std::right << std::setw(12)
                   << std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count() / double(n*m*100)
                   << " (ns/entry)";
@@ -57,8 +57,8 @@ TEST(Performance, MatmulTensorLayout)
         auto t2 = std::chrono::high_resolution_clock::now();
 
         std::cout << "   ("
-                  << std::right << std::setw(6) << n << ","
-                  << std::right << std::setw(6) << m << ") "
+                  << std::right << std::setw(8) << n << ","
+                  << std::right << std::setw(8) << m << ") "
                   << std::right << std::setw(12)
                   << std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count() / double(n*m*100)
                   << " (ns/entry)"
