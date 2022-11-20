0861299eccbf47b846f35cc753046b90b7e0d893
Matthias Moller <mmoelle1@gmail.com>
2022-11-20 15:25:02 +0100

    Extended geometry creator

--- include/creator.hpp
+++ include/creator.hpp
@@ -12,6 +12,9 @@
    file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
+#include <cstdlib>
+#include <ctime>
+
 #include <bspline.hpp>
 
 #pragma once
@@ -49,7 +52,7 @@ namespace iganet {
       : x0min_(0.0), x0max_(0.1),
         x1min_(0.9), x1max_(1.0)
     {
-      //std::seed();
+      std::srand(std::time(0));
     }
 
     /// Bounds constructor
@@ -58,11 +61,11 @@ namespace iganet {
     : x0min_(x0min), x0max_(x0max),
       x1min_(x1min), x1max_(x1max)
     {
-      //std::seed();
+      std::srand(std::time(0));
     }
     
     template<typename bspline_t>
-    auto next(bspline_t& obj)
+    auto& next(bspline_t& obj) const
     {
       static_assert(bspline_t::parDim() == 1 &&
                     bspline_t::geoDim() == 1,
@@ -70,8 +73,14 @@ namespace iganet {
 
       T x0 = x0min_ + (x0max_-x0min_) * T(std::rand()) / T(RAND_MAX);
       T x1 = x1min_ + (x1max_-x1min_) * T(std::rand()) / T(RAND_MAX);
-
-      return x1;
+      T xmin = std::min(x0,x1);
+      T xmax = std::max(x0,x1);
+      
+      obj.transform([xmin,xmax](const std::array<T,1> X){
+        return std::array<T,1>{ static_cast<T>( xmax*X[0] + xmin*(X[0]-T(1)) ) }; }
+        );
+      
+      return obj;
     }
 
     /// Returns a string representation of the IntervalCreator object
@@ -79,13 +88,82 @@ namespace iganet {
     {
       os << CreatorCore<T>::name() << "\n"
          << "(x0min = " << x0min_ << ", x0max = " << x0max_
-         << "; x1min = " << x1min_ << ", x1max = " << x1max_ << ")";
+         << "; x1min = " << x1min_ << ", x1max = " << x1max_
+         << ")";
     }
     
   private:
     T x0min_, x0max_, x1min_, x1max_;
   };
 
+  /// @brief Rectangle creator class
+  ///
+  /// This geometry creator generates a sequence of rectangles in the
+  /// specified bounds [xmin, xmax] x [ymin, ymax]
+  template<typename T>
+  class RectangleCreator : public CreatorCore<T>
+  {
+  public:
+    /// Default constructor
+    RectangleCreator()
+      : x0min_(0.0), x0max_(0.1),
+        x1min_(0.9), x1max_(1.0),
+        y0min_(0.0), y0max_(0.1),
+        y1min_(0.9), y1max_(1.0)
+    {
+      std::srand(std::time(0));
+    }
 
+    /// Bounds constructor
+    RectangleCreator(const T& x0min, const T& x0max,
+                     const T& x1min, const T& x1max,
+                     const T& y0min, const T& y0max,
+                     const T& y1min, const T& y1max)
+    : x0min_(x0min), x0max_(x0max),
+      x1min_(x1min), x1max_(x1max),
+      y0min_(y0min), y0max_(y0max),
+      y1min_(y1min), y1max_(y1max)
+    {
+      std::srand(std::time(0));
+    }
+    
+    template<typename bspline_t>
+    auto& next(bspline_t& obj) const
+    {
+      static_assert(bspline_t::parDim() == 2 &&
+                    bspline_t::geoDim() == 2,
+                    "Interval creator requires parDim=2 and geoDim=2");
+
+      T x0 = x0min_ + (x0max_-x0min_) * T(std::rand()) / T(RAND_MAX);
+      T x1 = x1min_ + (x1max_-x1min_) * T(std::rand()) / T(RAND_MAX);
+      T xmin = std::min(x0,x1);
+      T xmax = std::max(x0,x1);
+
+      T y0 = y0min_ + (y0max_-y0min_) * T(std::rand()) / T(RAND_MAX);
+      T y1 = y1min_ + (y1max_-y1min_) * T(std::rand()) / T(RAND_MAX);
+      T ymin = std::min(y0,y1);
+      T ymax = std::max(y0,y1);
+      
+      obj.transform([xmin,xmax,ymin,ymax](const std::array<T,2> X){
+        return std::array<T,2>{ static_cast<T>( xmax*X[0] + xmin*(X[0]-T(1)) ) }; }
+        );
+      
+      return obj;
+    }
+
+    /// Returns a string representation of the RectangleCreator object
+    virtual void pretty_print(std::ostream& os = std::cout) const
+    {
+      os << CreatorCore<T>::name() << "\n"
+         << "(x0min = " << x0min_ << ", x0max = " << x0max_
+         << "; x1min = " << x1min_ << ", x1max = " << x1max_
+         << "; y0min = " << y0min_ << ", y0max = " << y0max_
+         << "; y1min = " << y1min_ << ", y1max = " << y1max_
+         << ")";
+    }
+    
+  private:
+    T x0min_, x0max_, x1min_, x1max_, y0min_, y0max_, y1min_, y1max_;
+  };
   
 } // namespace iganet
