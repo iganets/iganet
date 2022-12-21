ef90e25afd9f4cf724c100fc854da274a1454ce6
Matthias Moller <mmoelle1@gmail.com>
2022-12-21 10:16:45 +0100

    Revised implementation of FunctionSpace class

--- include/functionspace.hpp
+++ include/functionspace.hpp
@@ -17,212 +17,91 @@
 #pragma once
 
 namespace iganet {
-
-  /// @brief Function space
-  ///
-  /// This class implements the functionality of a function space
-  template<typename real_t,
-           short_t TargetDim,
-           template<typename, short_t, short_t...> class bspline_t,
-           short_t...> class FunctionSpace;
-
-  /// @brief Function space (specialization for univariate domains)
-  template<typename real_t,
-           short_t TargetDim,
-           template<typename, short_t, short_t...> class bspline_t,
-           short_t degree0,
-           short_t cont0>
-  class FunctionSpace<real_t, TargetDim, bspline_t,
-                      degree0, cont0>
-    : public bspline_t<real_t, TargetDim, degree0>
-  {
-  private:
-    /// @brief Dimension of the parametric domain
-    static constexpr short_t domainDim_ = 1;
-
-    /// @brief Dimension of the physical domain
-    static constexpr short_t targetDim_ = TargetDim;
-
-  public:
-    /// @brief Constructor
-    FunctionSpace(const std::array<int64_t, domainDim_>& ncoeffs,
-                  enum init init = init::zeros)
-      : bspline_t<real_t, TargetDim, degree0>(ncoeffs, init)
-    {
-      std::cout << "Continuity=" << cont0 << std::endl;
-      // TODO: Perform knot insertion until reduced continuity is reached
-    }
-
-    /// @brief Returns the parametric dimension
-    inline static constexpr short_t domainDim()
-    {
-      return domainDim_;
-    }
-
-    /// @brief Returns the target dimension
-    inline static constexpr short_t targetDim()
-    {
-      return targetDim_;
-    }
-  };
-
-  /// @brief Function space (specialization for bivariate domains)
-  template<typename real_t,
-           short_t TargetDim,
-           template<typename, short_t, short_t...> class bspline_t,
-           short_t degree0, short_t degree1,
-           short_t cont0, short_t cont1>
-  class FunctionSpace<real_t, TargetDim, bspline_t,
-                      degree0, degree1, cont0, cont1>
-    : public bspline_t<real_t, TargetDim, degree0, degree1>
-  {
-  private:
-    /// @brief Dimension of the parametric domain
-    static constexpr short_t domainDim_ = 2;
-
-    /// @brief Dimension of the physical domain
-    static constexpr short_t targetDim_ = TargetDim;
-
-  public:
-    /// @brief Constructor
-    FunctionSpace(const std::array<int64_t, domainDim_>& ncoeffs,
-                  enum init init = init::zeros)
-      : bspline_t<real_t, TargetDim, degree0, degree1>(ncoeffs, init)
-    {
-      // TODO: Perform knot insertion until reduced continuity is reached
-    }
-
-    /// @brief Returns the parametric dimension
-    inline static constexpr short_t domainDim()
-    {
-      return domainDim_;
-    }
-
-    /// @brief Returns the target dimension
-    inline static constexpr short_t targetDim()
-    {
-      return targetDim_;
-    }
-  };
-
-  /// @brief Function space (specialization for trivariate domains)
-  template<typename real_t,
-           short_t TargetDim,
-           template<typename, short_t, short_t...> class bspline_t,
-           short_t degree0, short_t degree1, short_t degree2,
-           short_t cont0, short_t cont1, short_t cont2>
-  class FunctionSpace<real_t, TargetDim, bspline_t,
-                      degree0, degree1, degree2, cont0, cont1, cont2>
-    : public bspline_t<real_t, TargetDim, degree0, degree1, degree2>
-  {
-  private:
-    /// @brief Dimension of the parametric domain
-    static constexpr short_t domainDim_ = 3;
-
-    /// @brief Dimension of the physical domain
-    static constexpr short_t targetDim_ = TargetDim;
-
-  public:
-    /// @brief Constructor
-    FunctionSpace(const std::array<int64_t, domainDim_>& ncoeffs,
-                  enum init init = init::zeros)
-      : bspline_t<real_t, TargetDim, degree0, degree1, degree2>(ncoeffs, init)
-    {
-      // TODO: Perform knot insertion until reduced continuity is reached
-    }
-
-    /// @brief Returns the parametric dimension
-    inline static constexpr short_t domainDim()
-    {
-      return domainDim_;
-    }
-
-    /// @brief Returns the target dimension
-    inline static constexpr short_t targetDim()
-    {
-      return targetDim_;
-    }
-  };
-
-  /// @brief Function space (specialization for quadvariate domains)
-  template<typename real_t,
-           short_t TargetDim,
-           template<typename, short_t, short_t...> class bspline_t,
-           short_t degree0, short_t degree1, short_t degree2, short_t degree3,
-           short_t cont0, short_t cont1, short_t cont2, short_t cont3>
-  class FunctionSpace<real_t, TargetDim, bspline_t,
-                      degree0, degree1, degree2, degree3, cont0, cont1, cont2, cont3>
-    : public bspline_t<real_t, TargetDim, degree0, degree1, degree2, degree3>
-  {
-  private:
-    /// @brief Dimension of the parametric domain
-    static constexpr short_t domainDim_ = 4;
-
-    /// @brief Dimension of the physical domain
-    static constexpr short_t targetDim_ = TargetDim;
-
-  public:
-    /// @brief Constructor
-    FunctionSpace(const std::array<int64_t, domainDim_>& ncoeffs,
-                  enum init init = init::zeros)
-      : bspline_t<real_t, TargetDim, degree0, degree1, degree2, degree3>(ncoeffs, init)
-    {
-      // TODO: Perform knot insertion until reduced continuity is reached
-    }
-
-    /// @brief Returns the parametric dimension
-    inline static constexpr short_t domainDim()
-    {
-      return domainDim_;
-    }
-
-    /// @brief Returns the target dimension
-    inline static constexpr short_t targetDim()
-    {
-      return targetDim_;
-    }
-  };
-
-  /// @brief Alias for \f$ S_{p}^{q}\f$
-  template<typename real_t,
-           short_t TargetDim,
-           template<typename, short_t, short_t...> class bspline_t,
-           short_t degree, short_t cont = degree-1>
-  using S1 = FunctionSpace<real_t, TargetDim, bspline_t,
-                           degree,
-                           cont>;
   
-  /// @brief Alias for \f$ S_{p,p}^{p-1,p-1} :=
-  /// S_{p}^{p-1} \otimes S_{p}^{p-1} \f$
-  template<typename real_t,
-           short_t TargetDim,
-           template<typename, short_t, short_t...> class bspline_t,
-           short_t degree, short_t cont = degree-1>
-  using S2 = FunctionSpace<real_t, TargetDim, bspline_t,
-                           degree, degree,
-                           degree-1, degree-1>;
-  
-  /// @brief Alias for \f$ S_{p,p,p}^{p-1,p-1,p-1} :=
-  /// S_{p}^{p-1} \otimes S_{p}^{p-1} \otimes S_{p}^{p-1} \f$
-  template<typename real_t,
-           short_t TargetDim,
-           template<typename, short_t, short_t...> class bspline_t,
-           short_t degree, short_t cont = degree-1>
-  using S3 = FunctionSpace<real_t, TargetDim, bspline_t,
-                           degree, degree, degree,
-                           degree-1, degree-1, degree-1>;
-  
-  /// @brief Alias for \f$ S_{p,p,p,p}^{p-1,p-1,p-1,p-1} :=
-  /// S_{p}^{p-1} \otimes S_{p}^{p-1} \otimes S_{p}^{p-1} \otimes
-  /// S_{p}^{p-1} \f$
-  template<typename real_t,
-           short_t TargetDim,
-           template<typename, short_t, short_t...> class bspline_t,
-           short_t degree, short_t cont = degree-1>
-  using S4 = FunctionSpace<real_t, TargetDim, bspline_t,
-                           degree, degree, degree, degree,
-                           degree-1, degree-1, degree-1, degree-1>;
+  /// @brief Tensor-product function space
+  template<typename... BSpline_t>
+  class FunctionSpace : public std::tuple<FunctionSpace<BSpline_t>...>
+  {
+  private:
+    /// @brief Base class
+    using Base = std::tuple<FunctionSpace<BSpline_t>...>;
+    
+    /// @brief Dimension of the parametric domain
+    static constexpr std::array<short_t, sizeof...(BSpline_t)> domainDim_ = {BSpline_t::parDim()...};
+    
+    /// @brief Dimension of the physical domain
+    static constexpr std::array<short_t, sizeof...(BSpline_t)> targetDim_ = {BSpline_t::geoDim()...};
 
+    /// @brief Type of the boundary B-spline objects
+    using Boundary_t = std::tuple<Boundary<BSpline_t>...>;
+
+    /// @brief Boundary B-spline objects
+    Boundary_t boundary_;
+
+  public:   
+    /// @brief Constructor    
+    FunctionSpace(const std::array<int64_t, BSpline_t::parDim()>&... ncoeffs,
+                  enum init init = init::zeros)
+      : Base({ncoeffs, init}...),
+        boundary_({ncoeffs, init}...)
+    {}
+  };
+
+  /// @brief Function space 
+  template<typename BSpline_t>
+  class FunctionSpace<BSpline_t> : public BSpline_t
+  {
+  private:
+    /// @brief Dimension of the parametric domain
+    static constexpr short_t domainDim_ = BSpline_t::parDim();
+
+    /// @brief Dimension of the physical domain
+    static constexpr short_t targetDim_ = BSpline_t::geoDim();
+
+    /// @brief Type of the boundary B-spline objects
+    using Boundary_t = Boundary<BSpline_t>;
+
+    /// @brief Boundary B-spline objects
+    Boundary_t boundary_;
+    
+  public:
+    /// @brief Default constructor
+    FunctionSpace() = default;
+    
+    /// @brief Constructor
+    FunctionSpace(const std::array<int64_t, domainDim_>& ncoeffs,
+                  enum init init = init::zeros)
+      : BSpline_t(ncoeffs, init),
+        boundary_(ncoeffs, init)
+    {
+    }
+
+    /// @brief Returns the parametric dimension
+    inline static constexpr short_t domainDim()
+    {
+      return domainDim_;
+    }
+
+    /// @brief Returns the target dimension
+    inline static constexpr short_t targetDim()
+    {
+      return targetDim_;
+    }
+
+    /// @brief Returns a constant reference to the boundary B-spline object
+    inline const auto& boundary() const
+    {
+      return boundary_;
+    }
+
+    /// @brief Returns a non-constant reference to the boundary B-spline object
+    inline auto& boundary()
+    {
+      return boundary_;
+    }
+  };
+
+#if 0
   /// @brief Alias for Taylor-Hood like function space
   /// \f$ S_{p+1}^{p-1} \otimes S_{p}^{p-1} \f$
   template<typename real_t,
@@ -403,5 +282,6 @@ namespace iganet {
                                        degree,   degree,   degree,   degree+1,
                                        degree-1, degree-1, degree-1, degree>,
                          S4<real_t, 1, bspline_t, degree, degree-1>>;
-
+#endif
+  
 } // namespace iganet
