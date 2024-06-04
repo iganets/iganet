cf1196072e99ee575b7bad7bcfe438d9acf87711
Matthias Moller <mmoelle1@gmail.com>
2024-06-04 16:31:41 +0100

    Fixed missing override of virtual function

--- webapps/models/BSplineModel.hpp
+++ webapps/models/BSplineModel.hpp
@@ -259,6 +259,11 @@ public:
                 "type" : 2}])"_json;
   }
 
+    /// @brief Returns the model's parameters
+  nlohmann::json getParameters() const override {
+    return R"([])"_json;
+  }
+
   /// @brief Serializes the model to JSON
   nlohmann::json to_json(const std::string &component,
                          const std::string &attribute) const override {
