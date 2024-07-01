82bc6da8daa4c20d56fc08679e05e89ba00a1f94
Matthias Moller <mmoelle1@gmail.com>
2024-07-01 18:06:35 +0200

    Added iterator for G+Smo boundary sides

--- webapps/models/gismo/GismoModel.hpp
+++ webapps/models/gismo/GismoModel.hpp
@@ -21,6 +21,56 @@ namespace iganet {
 
 namespace webapp {
 
+/// @brief G+Smo boundary sides
+/// @{
+template <short_t>
+const std::initializer_list<gismo::boundary::side> GismoBoundarySides;
+
+template <>
+const std::initializer_list<gismo::boundary::side> GismoBoundarySides<1>{
+    gismo::boundary::side::west, gismo::boundary::side::east};
+
+template <>
+const std::initializer_list<gismo::boundary::side> GismoBoundarySides<2>{
+    gismo::boundary::side::west, gismo::boundary::side::east,
+    gismo::boundary::side::south, gismo::boundary::side::north};
+
+template <>
+const std::initializer_list<gismo::boundary::side> GismoBoundarySides<3>{
+    gismo::boundary::side::west,  gismo::boundary::side::east,
+    gismo::boundary::side::south, gismo::boundary::side::north,
+    gismo::boundary::side::front, gismo::boundary::side::back};
+
+template <>
+const std::initializer_list<gismo::boundary::side> GismoBoundarySides<4>{
+    gismo::boundary::side::west,  gismo::boundary::side::east,
+    gismo::boundary::side::south, gismo::boundary::side::north,
+    gismo::boundary::side::front, gismo::boundary::side::back,
+    gismo::boundary::side::stime, gismo::boundary::side::etime};
+/// @}
+
+/// @brief G+Smo boundary side names
+/// @{
+template <short_t>
+const std::initializer_list<std::string> GismoBoundarySideStrings;
+
+template <>
+const std::initializer_list<std::string> GismoBoundarySideStrings<1>{"west",
+                                                                     "east"};
+
+template <>
+const std::initializer_list<std::string> GismoBoundarySideStrings<2>{
+    "west", "east", "south", "north"};
+
+template <>
+const std::initializer_list<std::string> GismoBoundarySideStrings<3>{
+    "west", "east", "south", "north", "front", "back"};
+
+template <>
+const std::initializer_list<std::string> GismoBoundarySideStrings<4>{
+    "west", "east", "south", "north", "front", "back", "stime", "etime"};
+/// @}
+
 /// @brief G+Smo model
 template <class T> class GismoModel : public Model {
 
