8c49fbbb3304b6952ef038e0b5904435a76f6866
Matthias Möller <m.moller@tudelft.nl>
2024-09-30 16:09:03 +0200

    Suppress NVCC compiler warning

--- include/utils/tensorarray.hpp
+++ include/utils/tensorarray.hpp
@@ -181,6 +181,10 @@ inline std::ostream &operator<<(std::ostream &os,
   }
 #endif // defined(_WIN32)
 
+#ifdef __CUDACC__
+#pragma nv_diag_suppress 186
+#endif
+  
   os << *name_ << "(\n";
   for (std::size_t i = 0; i < N; ++i) {
     os << obj[i] << "\n";
@@ -189,6 +193,10 @@ inline std::ostream &operator<<(std::ostream &os,
       os << "[ " << obj[i].options() << " ]\n";
   }
 
+#ifdef __CUDACC__
+#pragma nv_diag_default 186
+#endif
+  
   os << ")";
 
   return os;
