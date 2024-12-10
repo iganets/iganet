c4baccfe4cf919bfbd650d2c034ff140c6115dd7
Matthias Moller <mmoelle1@gmail.com>
2024-12-10 22:32:48 +0100

    Updated Gismo model to multipatch geometries and removed global transform

--- webapps/models/gismo/GismoModel.hpp
+++ webapps/models/gismo/GismoModel.hpp
@@ -74,69 +74,29 @@ const std::initializer_list<std::string> GismoBoundarySideStrings<4>{
 /// @brief G+Smo base model
 template <class T> class GismoModel : public Model<T> {
 
-protected:
-  /// @brief Global offset vector
-  gismo::gsVector<T, 3> offset_;
-
-  /// @brief Global rotation vector
-  gismo::gsVector<T, 3> rotation_;
-
 public:
   /// @brief Default constructor
-  GismoModel() : transform_(gismo::gsMatrix<T, 4, 4>::Identity()) {
-    offset_.setZero();
-    rotation_.setZero();
-  }
+  GismoModel() {}
 
   /// @brief Destructor
   ~GismoModel() {}
 
   /// @brief Serializes the model to JSON
-  virtual nlohmann::json to_json(const std::string &component,
+  virtual nlohmann::json to_json(const std::string &patch,
+                                 const std::string &component,
                                  const std::string &attribute) const override {
 
-    if (component == "transform") {
-      nlohmann::json json;
-      json["matrix"] = utils::to_json(transform_, true);
-
-      return json;
-    }
-
-    else
-      return R"({ INVALID REQUEST })"_json;
+    return R"({ INVALID REQUEST })"_json;
   }
 
   /// @brief Updates the attributes of the model
-  virtual nlohmann::json updateAttribute(const std::string &component,
+  virtual nlohmann::json updateAttribute(const std::string &patch,
+                                         const std::string &component,
                                          const std::string &attribute,
                                          const nlohmann::json &json) override {
-    if (attribute == "transform") {
-      if (!json.contains("data"))
-        throw InvalidModelAttributeException();
-      if (!json["data"].contains("matrix"))
-        throw InvalidModelAttributeException();
 
-      auto matrix = json["data"]["matrix"].get<std::vector<iganet::real_t>>();
-
-      if (matrix.size() != 16)
-        throw IndexOutOfBoundsException();
-
-      size_t index(0);
-      for (const auto &entry : matrix) {
-        transform_(index % 4, index / 4) = entry;
-        index++;
-      }
-
-      return "{}";
-    }
-
-    else
-      return R"({ INVALID REQUEST })"_json;
+    return R"({ INVALID REQUEST })"_json;
   }
-
-protected:
-  /// @brief Global transformation matrix
-  gismo::gsMatrix<T, 4, 4, gismo::ColMajor> transform_;
 };
 
 } // namespace webapp
