fa91f31864c4c94178ea649ce545a991829994d2
Matthias Moller <mmoelle1@gmail.com>
2025-04-25 20:27:12 +0200

    Fixed bug in computation of hessian matrix

--- include/bspline.hpp
+++ include/bspline.hpp
@@ -4958,25 +4958,26 @@ public:
       return utils::BlockTensor<torch::Tensor, 1, 1>{
           torch::zeros_like(BSplineCore::coeffs_[0])};
     else {
-      auto hessu =
-          hess<memory_optimized>(xi, knot_indices, coeff_indices).slice(0);
+      std::array<utils::BlockTensor<torch::Tensor, BSplineCore::geoDim_, BSplineCore::geoDim_>, BSplineCore::geoDim_> hessu_array;
 
-      {
-        auto igradG =
-            igrad<memory_optimized>(G, xi, knot_indices, coeff_indices,
-                                    knot_indices_G, coeff_indices_G);
-        auto hessG = G.template hess<memory_optimized>(xi, knot_indices_G,
+      auto hessG = G.template hess<memory_optimized>(xi, knot_indices_G,
                                                        coeff_indices_G);
-        assert(igradG.cols() == hessG.slices());
-        for (short_t k = 0; k < hessG.slices(); ++k)
-          hessu -= igradG(0, k) * hessG.slice(k);
+      auto ijacG = ijac<memory_optimized>(G, xi, knot_indices, coeff_indices,
+                                            knot_indices_G, coeff_indices_G);
+
+      for (int component = 0; component < BSplineCore::geoDim_; ++component) {
+          auto hess_component = hess<memory_optimized>(xi, knot_indices, coeff_indices).slice(component);
+
+          for (short_t k = 0; k < hessG.slices(); ++k) {
+              hess_component -= ijacG(component, k) * hessG.slice(k);
+          }
+
+          auto jacInv = G.template jac<memory_optimized>(xi, knot_indices_G, coeff_indices_G).ginv();
+          hessu_array[component] = jacInv.tr() * hess_component * jacInv;
       }
 
-      auto jacInv =
-          G.template jac<memory_optimized>(xi, knot_indices_G, coeff_indices_G)
-              .ginv();
+      return hessu_array;
 
-      return jacInv.tr() * hessu * jacInv;
     }
   }
 
